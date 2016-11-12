35dca07c105a888fd6ef8699471df92596704634
Hugo Lucas <hugojlucas@gmail.com>
2016-11-11 19:14:11 -0800

    Added Mishael comments

--- src/EmployeeLog.java
+++ src/EmployeeLog.java
@@ -12,6 +12,14 @@ public class EmployeeLog {
     private int taskID;
     private int employeeID;
 
+    /**
+     * Creates an EmployeeLog with the given parameters.
+     * The employee ID is set to -1.
+     * @param ci time employee has clocked in
+     * @param co time the user has clocked out
+     * @param ld the date the task was worked on my employee
+     * @param id the ID of the task employee worked on
+     */
     public EmployeeLog(Time ci, Time co, Date ld, int id){
         this.clockIn = ci;
         this.clockOut = co;
@@ -21,6 +29,15 @@ public class EmployeeLog {
         this.employeeID = -1;
     }
 
+    /**
+     * Creates an EmployeeLog with the given parameters.
+     * The employee ID number is given.
+     * @param ci time employee has clocked in
+     * @param co time the user has clocked out
+     * @param ld the date the task was worked on my employee
+     * @param id the ID of the task employee worked on
+     * @param eid the ID number of the employee
+     */
     public EmployeeLog(Time ci, Time co, Date ld, int id, int eid){
         this.clockIn = ci;
         this.clockOut = co;
@@ -30,6 +47,12 @@ public class EmployeeLog {
         this.employeeID = eid;
     }
 
+    /**
+     * Displays the time of clock in and clock out, the date, the task that was
+     * worked on, and, if available, the ID number of the employee that worked
+     * on the task.
+     * @return the string to be printed out
+     */
     public String printLog(){
         String out = null;
         String clockProxy = null;
--- src/userLogIn.java
+++ src/userLogIn.java
@@ -11,6 +11,9 @@ public class userLogIn {
     private int managerStatus;
     private ArrayList<Employee> list;
 
+    /**
+     * Sets default value of logged in user's information
+     */
     public userLogIn(){
         this.workStatus = -1;
         this.employeeNumber = -1;
@@ -18,6 +21,15 @@ public class userLogIn {
         this.list = null;
     }
 
+    /**
+     * Logs the user in by making a call to the DB_Reader's login_user method
+     * which checks if information provided by the user is valid.
+     * employeeNumber = -1 if login information is invalid. employeeNumber = -2
+     * if any other error has occurred. employeeNumber is set to the employee's
+     * ID number if the login information is valid.
+     * @param username the username inputted by the user
+     * @param password the password inputted by the user
+     */
     public void login(String username, String password){
         DB_Reader reader = new DB_Reader();
 
@@ -34,11 +46,23 @@ public class userLogIn {
         }
     }
 
+    /**
+     * Returns the information of the current user that is logged in.
+     * @return an int array which holds an employee's ID number, work status,
+     * and manager status
+     */
     public int[] results(){
         int retArray[] = {this.employeeNumber, this.workStatus, this.managerStatus};
         return retArray;
     }
 
+    /**
+     * Populates the drop down list with projects that the employee is
+     * allowed to work on.
+     * @param num the employee's ID number
+     * @return an EmployeeProjectTaskMap object which holds different
+     * projects and tasks.
+     */
     public EmployeeProjectTaskMap populateDropDown(int num){
         DB_Reader reader = new DB_Reader();
         EmployeeProjectTaskMap map = reader.projectTaskMap(num);
@@ -46,6 +70,13 @@ public class userLogIn {
         return map;
     }
 
+    /**
+     * Gets the name of the projects mapped into the EmployeeProjectTaskMap
+     * object and stores them into an ArrayList of Strings.
+     * @param map holds projects that the employee is allowed to work on
+     * @return a list of the names of all projects that the employee can
+     * work on
+     */
     public ArrayList<String> projectList(EmployeeProjectTaskMap map){
         ArrayList<EmployeeProject> p = map.getProjects();
         ArrayList<String> returnList = new ArrayList<>(p.size());
@@ -56,6 +87,13 @@ public class userLogIn {
         return returnList;
     }
 
+    /**
+     * Gets the name of the tasks that are associated with a project and stores
+     * them into an ArrayList.
+     * @param projName the name of the project
+     * @param map holds tasks that the employee is allowed to work on
+     * @return a list of the names of all tasks that is associated with a project
+     */
     public ArrayList<String> tasksInProject(String projName, EmployeeProjectTaskMap map){
         ArrayList<String> taskList = new ArrayList<>();
 
@@ -65,6 +103,11 @@ public class userLogIn {
         return taskList;
     }
 
+    /**
+     * Gets the ID of the project and task the current user is working on.
+     * @return an array of int holding the ID of the project and task of the
+     * current user
+     */
     public int [] taskAndProjectIDs(){
         DB_Reader reader = new DB_Reader();
         int IDs[] = reader.getCurrentEmployeeTaskAndProject(employeeNumber);
@@ -72,12 +115,21 @@ public class userLogIn {
         return IDs;
     }
 
+    /**
+     * Used by a manager to get a list of all employees.
+     * @return an ArrayList of all employees currently working in the company
+     */
     public ArrayList<Employee> getAllEmployees(){
         DB_Reader reader = new DB_Reader();
         list = reader.allEmployees();
         return list;
     }
 
+    /**
+     * Used by a manager to get the ID number of an employee.
+     * @param name the name of the employee
+     * @return the ID number of an employee
+     */
     public int getEmployeeNumberByName(String name){
         if (list != null)
             for(Employee e: list)
